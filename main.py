3d64fa94b8a18a23886d47159fe00afa677fd9aa
rodal22 <89620639+rodal22@users.noreply.github.com>
2024-12-31 11:56:23 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# Importar
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    # Variables que permiten calcular el consumo energético de los aparatos
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+# La primera página
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# La segunda página
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+                           
+@app.route('/lights/eco-light')
+def eco_light():
+    return render_template('eco_light.html')                
+
+# La tercera página
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Cálculo
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+app.run(debug=True)
